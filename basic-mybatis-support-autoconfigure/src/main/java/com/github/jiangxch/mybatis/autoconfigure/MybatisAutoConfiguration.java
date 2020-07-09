55da8bb25f68af3acce7d6acdf047c4792db56fb
蒋小川 <jiangxch8355@fxiaoke.com>
2020-07-09 20:22:40 +0800

    添加springboot依赖

---
+++ basic-mybatis-support-autoconfigure/src/main/java/com/github/jiangxch/mybatis/autoconfigure/MybatisAutoConfiguration.java
@@ -0,0 +1,12 @@
+package com.github.jiangxch.mybatis.autoconfigure;
+
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * @author: jiangxch
+ * @date: 2020/7/9 20:02
+ */
+@Configuration
+public class MybatisAutoConfiguration {
+
+}
