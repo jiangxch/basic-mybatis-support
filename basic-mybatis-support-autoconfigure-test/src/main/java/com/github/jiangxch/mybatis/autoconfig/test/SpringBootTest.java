391f0972f99999cc2901e7b8345774b89b75f3ba
蒋小川 <jiangxch8355@fxiaoke.com>
2020-07-15 19:36:54 +0800

    添加springboottest

--- basic-mybatis-support-autoconfigure-test/src/main/java/com/github/jiangxch/mybatis/autoconfig/test/SpringBootTest.java
+++
@@ -1,8 +0,0 @@
-package com.github.jiangxch.mybatis.autoconfig.test;
-
-/**
- * @author: jiangxch
- * @date: 2020/7/15 上午12:39
- */
-public class SpringBootTest {
-}
---
+++ basic-mybatis-support-autoconfigure-test/src/main/test/com/github/jiangxch/mybatis/autoconfig/test/SpringBootTest.java
@@ -0,0 +1,15 @@
+package com.github.jiangxch.mybatis.autoconfig.test;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author: jiangxch
+ * @date: 2020/7/15 上午12:39
+ */
+@SpringBootApplication
+public class SpringBootTest {
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootTest.class,args);
+    }
+}
