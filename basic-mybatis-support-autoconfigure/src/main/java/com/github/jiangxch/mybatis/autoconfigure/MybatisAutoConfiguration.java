de22d2720791275840993a2aeda688a97f408da9
jiangxch <jiangxch8355@fxiaoke.com>
2020-07-18 16:12:51 +0800

    update resource path

--- basic-mybatis-support-autoconfigure/src/main/java/com/github/jiangxch/mybatis/autoconfigure/MybatisAutoConfiguration.java
+++ basic-mybatis-support-autoconfigure/src/main/java/com/github/jiangxch/mybatis/autoconfigure/MybatisAutoConfiguration.java
@@ -1,25 +1,18 @@
 package com.github.jiangxch.mybatis.autoconfigure;
 
-import com.alibaba.druid.pool.DruidDataSource;
 import com.github.jiangxch.mybatis.core.spring.DynamicDataSource;
 import com.google.common.collect.Iterables;
 import org.apache.ibatis.session.SqlSessionFactory;
 import org.mybatis.spring.SqlSessionFactoryBean;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.autoconfigure.AutoConfigureAfter;
-import org.springframework.boot.autoconfigure.condition.ConditionalOnBean;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnClass;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
-import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
-import org.springframework.boot.autoconfigure.jdbc.DataSourceProperties;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.core.annotation.Order;
 import org.springframework.core.io.ClassPathResource;
 import org.springframework.core.io.Resource;
 
-import javax.sql.DataSource;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -39,7 +32,7 @@ public class MybatisAutoConfiguration {
     @ConditionalOnMissingBean
     public SqlSessionFactoryBean sqlSessionFactoryBean(){
         SqlSessionFactoryBean factoryBean = new SqlSessionFactoryBean();
-        String configLocation = "META-INF/basic-mybatis-support-config.properties";
+        String configLocation = "basic-mybatis-support-config";
         if (mybatisProperties.getConfigLocation() != null
                 && mybatisProperties.getConfigLocation().length() != 0) {
             configLocation = mybatisProperties.getConfigLocation();
